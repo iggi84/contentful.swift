8657434fae5a9df69e0323e8bcb1ad1602c9b293
Igor Vojinovic <igor.vojinovic@appculture.com>
2016-09-09 15:05:32 +0200

    Package Swift update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/neonichu/Decodable", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/neonichu/Interstellar", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/neonichu/Clock", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/iggi84/Clock.git", majorVersion: 0, minor: 1),
     ]
 )
