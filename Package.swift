5f14202efb016e44622b64b4cc73910f2674ec2d
Igor Vojinovic <igor.vojinovic@appculture.com>
2016-09-09 14:37:12 +0200

    Updated swift package for personal link on git hub

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/neonichu/Decodable", majorVersion: 0),
         .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 1),
-        .Package(url: "https://github.com/neonichu/Clock", majorVersion: 0),
+        .Package(url: "https://github.com/iggi84/Clock.git", majorVersion: 0),
     ]
 )
